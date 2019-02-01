8935939aa527b4c6ced4041b310f80bcb3348a60
lilkgb <mcleanrs@comcast.net>
2019-02-01 14:35:05 -0800

    found error on html. script for scripts.js was set on type instead of src. change and scripts working properly now

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,33 @@
-function PizzaOrder(size, crust, toppings) {
-  this.size = size;
-  this.crust = crust;
-  this.toppings = toppings;
-}
-var size = {
-  sizeOptions: ["Small", "Medium", "Thin"],
-  sizePrices: [7, 10, 13]
-};
-var crust = {
-  crustOptions: ["Regular", "Cheesy", "Thin"],
-  crustPrices: [3, 5, 6]
-};
-var toppings = {
-  toppingOptions: ["Pepperoni", "Jalapeno", "Onion", "BellPepper"],
-  toppingPrices: [2, 5, 3, 4]
-};
-
+// function PizzaOrder(size, crust, toppings) {
+//   this.size = size;
+//   this.crust = crust;
+//   this.toppings = toppings;
+//   this.price = 0;
+// }
+// var size = {
+//   sizeOptions: ["Small", "Medium", "Thin"],
+//   sizePrices: [7, 10, 13]
+// };
+// var crust = {
+//   crustOptions: ["Regular", "Cheesy", "Thin"],
+//   crustPrices: [3, 5, 6]
+// };
+// var toppings = {
+//   toppingOptions: ["Pepperoni", "Jalapeno", "Onion", "BellPepper"],
+//   toppingPrices: [2, 5, 3, 4]
+// };
+// function addPizzaOrder(size, crust, toppings) {
+//   newPizzaOrder = new PizzaOrder(size, crust, toppings);
+//   calculatePrice();
+//   finalOrder.totalPrice += newPizzaOrder.price;
+//   finalOrder.pizzas.push(newPizzaOrder);
+//   finalOrder.orderedPizzas += 1;
+// }
+// function calculatePrice() {
+//   newPizzaOrder.price =+ size.sizePrices[size.sizeOptions.indexOf(newPizzaOrder.size)] + crust.crustPrices[crust.crustOptions.indexOf(newPizzaOrder.crust)];
+// }
 $(document).ready(function() {
-  $("#pizza-maker").submit(function(event) {
-    event.preventDefault();
-    var newPizzaSize = $("input:radio[name=pizza-size]:checked").val();
-    var newpizzaCrust = $("input:radio[name=pizza-crust]:checked").val();
-    var newPizzaToppings = [];
-    $("input:checkbox[name=toppings]:checked").each(function(){
-       newPizzaToppings.push($(this).val());
-     });
+  $("#pizza-maker").submit(function() {
+    alert("hello");
   });
 });
