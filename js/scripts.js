0649ed13bf488665666c6dc6d5edc2652cc2a140
lilkgb <mcleanrs@comcast.net>
2019-02-01 13:47:58 -0800

    add pizzaOrder constructor and var for size, crust, toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
-function Pizza(size, toppings, cost) {
+function PizzaOrder(size, crust, toppings) {
   this.size = size;
+  this.crust = crust;
   this.toppings = toppings;
-  this.cost = cost;
 }
+var size = {
+  sizeOptions: ["Small", "Medium", "Thin"],
+  sizePrices: [7, 10, 13]
+};
+var crust = {
+  crustOptions: ["Regular", "Cheesy", "Thin"],
+  crustPrices: [3, 5, 6]
+};
+var toppings = {
+  toppingOptions: ["Pepperoni", "Jalapeno", "Onion", "BellPepper"],
+  toppingPrices: [2, 5, 3, 4]
+};
 
 $(document).ready(function() {
   $("#custom-pizza").submit(function(event) {
