9d04b86a1c354ee02ccf2496a40559cb792472e4
lilkgb <mcleanrs@comcast.net>
2019-02-01 16:18:50 -0800

    got calculate price to work properly. when add pizza size, crust and toppings it all adds up correctly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,55 @@
-// function PizzaOrder(size, crust, toppings) {
-//   this.size = size;
-//   this.crust = crust;
-//   this.toppings = toppings;
-//   this.price = 0;
-// }
-// var size = {
-//   sizeOptions: ["Small", "Medium", "Thin"],
-//   sizePrices: [7, 10, 13]
-// };
-// var crust = {
-//   crustOptions: ["Regular", "Cheesy", "Thin"],
-//   crustPrices: [3, 5, 6]
-// };
-// var toppings = {
-//   toppingOptions: ["Pepperoni", "Jalapeno", "Onion", "BellPepper"],
-//   toppingPrices: [2, 5, 3, 4]
-// };
-// function addPizzaOrder(size, crust, toppings) {
-//   newPizzaOrder = new PizzaOrder(size, crust, toppings);
-//   calculatePrice();
-//   finalOrder.totalPrice += newPizzaOrder.price;
-//   finalOrder.pizzas.push(newPizzaOrder);
-//   finalOrder.orderedPizzas += 1;
-// }
-// function calculatePrice() {
-//   newPizzaOrder.price =+ size.sizePrices[size.sizeOptions.indexOf(newPizzaOrder.size)] + crust.crustPrices[crust.crustOptions.indexOf(newPizzaOrder.crust)];
-// }
+function PizzaOrder(size, crust, toppings) {
+  this.pizzaSize = size;
+  this.pizzaCrust = crust;
+  this.pizzaToppings = toppings;
+  this.price = 0;
+}
+function OrderTotal () {
+  this.pizzas=[];
+  this.totalPrice=0;
+  this.orderedPizzas=0;
+}
+var sizes = {
+  sizeOptions: ["Small", "Medium", "Thin"],
+  sizePrices: [7, 10, 13]
+};
+var crusts = {
+  crustOptions: ["Regular", "Cheesy", "Thin"],
+  crustPrices: [3, 5, 6]
+};
+var toppings = {
+  toppingOptions: ["Pepperoni", "Jalapeno", "Onion", "BellPepper"],
+  toppingPrices: [2, 5, 3, 4]
+};
+function addPizzaOrder(size, crust, toppings) {
+  newPizzaOrder = new PizzaOrder(size, crust, toppings);
+  calculatePrice();
+  finalOrder.totalPrice += newPizzaOrder.price;
+  finalOrder.pizzas.push(newPizzaOrder);
+  finalOrder.orderedPizzas += 1;
+}
+function calculatePrice(){
+
+  newPizzaOrder.price +=  sizes.sizePrices[sizes.sizeOptions.indexOf(newPizzaOrder.pizzaSize)] + crusts.crustPrices[crusts.crustOptions.indexOf(newPizzaOrder.pizzaCrust)];
+
+  for (var i = 0; i < newPizzaOrder.pizzaToppings.length; i++) {
+  var toppingPrice = toppings.toppingOptions.indexOf(newPizzaOrder.pizzaToppings[i]);
+  newPizzaOrder.price += toppings.toppingPrices[toppingPrice];
+  }
+}
+
 $(document).ready(function() {
-  $("#pizza-maker").submit(function() {
-    alert("hello");
+  finalOrder = new OrderTotal();
+  $("#pizza-maker").submit(function(event) {
+    event.preventDefault();
+    var newPizzaSize = $("input:radio[name=pizza-size]:checked").val();
+    var newpizzaCrust = $("input:radio[name=pizza-crust]:checked").val();
+    var newPizzaToppings = [];
+      $("input:checkbox[name=toppings]:checked").each(function(){
+       newPizzaToppings.push($(this).val());
+      });
+    var donePizza = addPizzaOrder(newPizzaSize, newpizzaCrust, newPizzaToppings);
+    console.log(donePizza);
+    console.log(finalOrder);
   });
 });
